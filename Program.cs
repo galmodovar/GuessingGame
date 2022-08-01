2030cd5b42b99c3f59dc1641a5f3cadce80fde02
galmodovar <galmodovar1@gmail.com>
2022-08-01 14:44:34 -0500

    initial commit phase 1 and 2

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int secretNumber = 42;
+
+            Console.WriteLine("Give me a number between 1 and 50!:");
+
+            string res = Console.ReadLine();
+            int num = int.Parse(res);
+
+            if (num == secretNumber)
+            {
+                Console.WriteLine("You don't suck.");
+            }
+            else {
+                Console.WriteLine("You suck.");
+            }
+
+        }
+    }
+}
