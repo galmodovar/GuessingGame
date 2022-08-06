593727c5042073a687fd8a664c6774be3643adab
galmodovar <galmodovar1@gmail.com>
2022-08-06 09:58:25 -0500

    Phase 3

--- Program.cs
+++ Program.cs
@@ -7,19 +7,33 @@ namespace GuessingGame
         static void Main(string[] args)
         {
             int secretNumber = 42;
+            int guesses = 0;
 
             Console.WriteLine("Give me a number between 1 and 50!:");
 
             string res = Console.ReadLine();
             int num = int.Parse(res);
 
+            while (guesses < 4) 
+            {
             if (num == secretNumber)
             {
                 Console.WriteLine("You don't suck.");
+                break;
             }
             else {
-                Console.WriteLine("You suck.");
+                guesses++;
+                Console.WriteLine($"You suck. {5 - guesses} guesses left.");
+                res = Console.ReadLine();
+                num = int.Parse(res);
             }
+            }
+
+            if (num != secretNumber)
+            {
+                Console.WriteLine("All out of guesses.");
+            }
+
 
         }
     }
